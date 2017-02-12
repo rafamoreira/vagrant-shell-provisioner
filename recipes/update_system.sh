9ab202b2040a95135bd43080d2f48ed89edce09c
Rafael Moreira <i@rafaelmoreira.net>
2017-02-12 20:05:30 -0300

    Simplify update

--- recipes/update_system.sh
+++ recipes/update_system.sh
@@ -2,6 +2,4 @@
 
 sudo apt-get update
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
-
+DEBIAN_FRONTEND=noninteractive apt-get -y -o DPkg::options::="--force-confdef" -o DPkg::options::="--force-confold" dist-upgrade
