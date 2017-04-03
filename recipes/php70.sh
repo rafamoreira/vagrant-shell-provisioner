09643daa1c34fd323624580cf9a638c056821d78
rafael moreira <i@rafaelmoreira.net>
2017-04-03 13:25:11 -0300

    add mysql extension to php recipe

--- recipes/php70.sh
+++ recipes/php70.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 if [ ! -f /home/vagrant/provision/php70 ]; then
 
-  sudo apt-get install -y -qq php7.0 libapache2-mod-php7.0  
+  sudo apt-get install -y -qq php7.0 libapache2-mod-php7.0  php7.0-mysql
 
   su -c "touch /home/vagrant/provision/php70" vagrant
 
